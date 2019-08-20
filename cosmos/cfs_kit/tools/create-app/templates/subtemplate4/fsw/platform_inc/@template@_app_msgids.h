a41e6ad080670b64e05a44e8cf80b530e99b23a5
Danilo Miranda <danilojfmiranda@gmail.com>
2019-08-20 09:41:32 -0300

    subtemplate 4 replicated from subtemplate 3

---
+++ cosmos/cfs_kit/tools/create-app/templates/subtemplate4/fsw/platform_inc/@template@_app_msgids.h
@@ -0,0 +1,33 @@
+/************************************************************************
+** File:
+**   $Id: @template@_app_msgids.h  $
+**
+** Purpose: 
+**  Define @Template@ App  Message IDs
+**
+** Notes:
+**
+**
+*************************************************************************/
+#ifndef _@template@_app_msgids_h_
+#define _@template@_app_msgids_h_
+
+/*
+** Command Message IDs
+*/
+
+#define  @TEMPLATE@_CMD_MID           0x1883
+#define  @TEMPLATE@_SEND_HK_MID       0x1885  /* Same as KIT_CI to simplify integration */
+#define  @TEMPLATE@_WAKE_UP_MID       0x18AB /* Same as SC_1Hz (0x18AB) */
+
+/*
+** Telemetry Message IDs
+*/
+
+#define  @TEMPLATE@_HK_TLM_MID     0x0883
+
+#endif /* _@template@_app_msgids_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
